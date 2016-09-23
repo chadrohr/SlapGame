26730c2903ea910f2c891c60fe8f22792d83c353
Chad Rohr <chadrohr@yahoo.com>
2016-09-23 14:14:55 -0600

    corrected health to zero

--- game.js
+++ game.js
@@ -8,16 +8,26 @@ function onSlap(){
   update()
   return health
 }
+function onKick(){
+    health -= kick
+}
 
 function onPunch(){
   health -= punch
   update()
   return health
 }
-
+function onReset(){
+    health = 100
+    update()
+    return health
+}
 
 function update(){
   var healthElem = document.getElementById('health')
+  if(health <= 0 ){
+      health = 0
+  }
   healthElem.innerHTML = health
 }
 
