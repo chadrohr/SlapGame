e1a3e4661b65a4ce092bd133175d6a7567e3676b
Chad Rohr <chadrohr@yahoo.com>
2016-09-25 10:55:33 -0600

    got the red working

--- game.js
+++ game.js
@@ -1,32 +1,33 @@
 var health = 100;
 var slap = 1;
-var punch = 10;
-var kick = 20;
-var players = {}
-function Players(name, health,slap, punch, kick){
-    this.name = name;
-    this.health = health;
-    this.slap = slap;
-    this.punch = punch;
-    this.kick = kick;
-    this.draw = function(){
+var punch = 5;
+var kick = 10;
 
-    }
-}
-var Players = {
-    Player1: new 
-}
-function Items(name, modifier, description){
-    this.name = name;
-    this.modifier = modifier;
-    this.description = description;
-    this.draw = function(){
+//var players = {}
+// function Players(name, health, slap, punch, kick){
+//     this.name = name;
+//     this.health = health;
+//     this.slap = slap;
+//     this.punch = punch;
+//     this.kick = kick;
+//     this.draw = function(){
 
-    }
-}
-var Items = {
-    sheild:new Item("Shield", 1, 'That was a blocked')
-}
+//     }
+// }
+// var Players = {
+//     Player1: new 
+// }
+// function Items(name, modifier, description){
+//     this.name = name;
+//     this.modifier = modifier;
+//     this.description = description;
+//     this.draw = function(){
+
+//     }
+// }
+// var Items = {
+//     sheild:new Item("Shield", 1, 'That was a blocked')
+// }
 
 function onSlap(){
   health -= slap
@@ -45,21 +46,21 @@ function onKick(){
 }
 function onReset(){
     health = 100
+    document.getElementById('health').style.color= "white";
     update()
     return health
 }
 function update(){
-  var healthElem = document.getElementById('health')
-  if(health <= 0 ){
-      health = 0
- // }else{
- //     if(health <= 20){
-          //change color to red
+ var healthElem = document.getElementById('health');
+  { 
+    if(health <= 20){
+    document.getElementById('health').style.color= "red";  
       }
-  }
-  healthElem.innerHTML = health
+      if(health <= 0 ){
+     health = 0
+ } 
+    healthElem.innerHTML = health;
+}
 }
 
-//console.log(health)
-//console.log(onSlap())
   
